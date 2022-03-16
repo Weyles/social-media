f02240a4837f7c14bc036c9ec9e5ffc962dfb586
Weyles <SasoriTheme@gmail.com>
2022-03-16 13:57:45 +0200

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="header">
+       <div>
+        <img alt="img" src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"/>
+       </div>
+      </header>
+  )
+}
